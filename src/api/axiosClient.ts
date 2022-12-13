1e82d135eb025b1ff4020e024d6470ebc493d033
duytran0209 <tranduy10a@gmail.com>
2022-12-13 16:17:47 +0700

    Update axiosClient.ts

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -23,6 +23,7 @@ axiosClient.interceptors.response.use(
   function (response: AxiosResponse) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    // response.data
     return response;
   },
   function (error) {
